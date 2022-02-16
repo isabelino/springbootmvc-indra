a25526483b3a543759ce9d3e3ede9daeec17f892
rolando <isa.klopez@hotmail.com>
2022-02-16 17:29:07 +0100

    springboot webmvc

---
+++ appwebmvc/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO empleados (nombre,apellido,email,telefono) VALUES ('Jose','Garcia','jg@email.com',214512);
+INSERT INTO empleados (nombre,apellido,email,telefono) VALUES ('Carlos','Perez','cp@email.com',545412);
+INSERT INTO empleados (nombre,apellido,email,telefono) VALUES ('Maria','Orue','mo@email.com',212112);
+INSERT INTO empleados (nombre,apellido,email,telefono) VALUES ('Gloria','Gomez','gg@email.com',814512);
+INSERT INTO empleados (nombre,apellido,email,telefono) VALUES ('Enrrique','Gutierrez','eg@email.com',654512);
